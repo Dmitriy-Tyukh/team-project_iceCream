c7157b956343c03c4f8b8c0dc9642fda4548d3c7
djbob2000 <djbob2000@gmail.com>
2022-08-14 22:17:09 +0300

    fix script mob menu and css header

--- src/js/mobile-menu-scrolllock.js
+++ src/js/mobile-menu-scrolllock.js
@@ -16,7 +16,8 @@
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
+  // closeMenuBtn.addEventListener('click', toggleMenu);
+  mobileMenu.addEventListener('click', toggleMenu);
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
