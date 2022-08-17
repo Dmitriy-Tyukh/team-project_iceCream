0214f728a084e329a06fcee457103fe0294ec49e
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2022-08-17 12:20:20 +0400

    quality Class

--- src/js/storeApi.js
+++ src/js/storeApi.js
@@ -1,3 +1,5 @@
+import { debounce } from 'debounce';
+
 const refs = {
   form: document.querySelector('.form'),
   productQuantity: document.querySelectorAll('.quantity'),
@@ -11,20 +13,87 @@ const refs = {
 class ProductsQuantity {
   constructor(selector) {
     this.refs = this.getRefs(selector);
+    this._totalQuantity = this.totalQuantity;
+
+    // incrisebutton listener
+    this.refs.incrisebutton.addEventListener('click', () => {
+      this.inc();
+      // return this.refs.inputvalue.value;
+      // console.log(this.totalQuantity);
+      return this.totalQuantity;
+    });
+
+    // decrisebutton listener
+    this.refs.decrisebutton.addEventListener('click', () => {
+      this.dec();
+      // return this.refs.inputvalue.value;
+
+      console.log(this.totalQuantity);
+    });
+
+    // input listener
+    this.refs.inputvalue.addEventListener('input', () => {
+      // return this.refs.inputvalue.value;
+      this.totalQuantity = Number(this.refs.inputvalue.value);
+      console.log(this.totalQuantity);
+    });
   }
 
   getRefs(selector) {
-    const refs = {};
-    refs.elements = [...document.querySelector(selector).children].reduce(
-      (acc, el) => el,
+    const refs = [...document.querySelector(selector).children].reduce(
+      (refs, el) => {
+        const key = Object.keys(el.dataset).join();
+        refs[key] = el;
+        return refs;
+      },
       {}
     );
-    console.log('refs.el :>> ', refs.elements);
-    // return refs;
+    // console.log('refs :>> ', refs);
+    return refs;
+  }
+
+  inc() {
+    if (this._totalQuantity >= 100) {
+      this._totalQuantity = 100;
+      return;
+    }
+    this._totalQuantity += 1;
+    this.refs.inputvalue.value = this._totalQuantity;
+  }
+
+  dec() {
+    if (this.totalQuantity < 0) {
+      return;
+    }
+
+    this._totalQuantity -= 1;
+    this._totalQuantity <= 0
+      ? (this.refs.inputvalue.value = '')
+      : (this.refs.inputvalue.value = this.totalQuantity);
+  }
+
+  get totalQuantity() {
+    if (this.refs.inputvalue.value === '') {
+      this._totalQuantity = 0;
+    }
+    return this._totalQuantity;
+  }
+  set totalQuantity(newQuantity) {
+    if (newQuantity >= 100) {
+      this._totalQuantity = 100;
+      this.refs.inputvalue.value = this._totalQuantity;
+
+      return;
+    }
+    this._totalQuantity = newQuantity;
   }
 }
 
 // const isecream = new ProductsQuantity('.quantity');
 const isecream = new ProductsQuantity('[data-quantity="icecream"]');
 
-// console.log('isecream :>> ', isecream);
+console.log('isecream :>> ', isecream.totalQuantity);
+
+isecream.refs.inputvalue.addEventListener('change', () => {
+  console.log(isecream.refs.inputvalue.value);
+});
